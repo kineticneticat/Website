d0b2fdd8015f6fafa751c49c386ad3e539424784
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2022-02-04 22:38:10 +0000

    projects page flexbox

--- main.py
+++ main.py
@@ -227,5 +227,5 @@ def prpo():
     # except:
     #     return 'this page broke lol'
 
-
-app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
